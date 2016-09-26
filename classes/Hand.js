f82f24e70795527ab57bb29c2786ebc42c1eae8e
fryymann715 <ideans715@gmail.com>
2016-09-26 15:34:27 -0700

    Created Hand class, incomplete Player class added.

---
+++ classes/Hand.js
@@ -0,0 +1,11 @@
+class Hand {
+  constructor () {
+    this.cards = []
+    this.currentBet = 0
+  }
+
+  const addCard = ( card ) => {
+    this.cards.push( card )
+  }
+
+}
--- classes/Player.js
+++ classes/Player.js
@@ -1,22 +1,69 @@
 class Player {
     constructor(options) {
-    this.hand = options.hand 
-    this.bank = options.bank
+    this.hand = // Instance of Hand class
+    this.bank = 100
     this.seat = options.seat
     }
-    
-    hit(),
-    
-    bet(),
-    
-    stay(),
-    
-    doubleDown(),
-    
-    split(),
-    
-    buyInsurance(),
-    
-    surrender(),
+
+    // Player decides to add a new card to their hand.
+    const hit = () => {
+      // invokes dealCard() on the dealer which invokes addCard() on the hand.
+    },
+
+    // Takes funds from player's bank and adds it the the Hand's bet variable.
+    const bet = ( option ) => {
+      if ( option = 'all-in' ) {
+        // Add this.bank to Hand's bet.
+      } else {
+        this.currentBet = option
+        this.bank -= option
+      }
+    }
+
+    // If player wishes to sit out a round.
+    const stay = () => {
+
+    }
+
+    // Doubles the player's current bet
+    const doubleDown = () => {
+      let doubledBet = this.currentBet * 2
+      this.bank -= doubledBet
+    },
+
+    // If player is deal two of the same cards, splits hand into two hands.
+    const split = () => {
+      if ( this.hand[0].rank === this.hand[1].rank ) {
+        let hand1 = [this.hand[0]]
+        let hand2 = [this.hand[1]]
+        this.hand = [hand1, hand2]
+      } else {
+        return "Can only split when deal two of the same cards."
+      }
+    }
+
+    // Process of buy 'insurance' incase dealer gets natural blackjack
+    const buyInsurance = () => {
+
+    }
+
+    //
+    const surrender = () => {
+
+    }
+
+    // Calls the Card.value() function to total the value of Player's hand.
+    const handValue = (this.hand) => {
+      let total = 0
+      for ( let card in hand ) {
+        total += card.value()
+      }
+      return total
+    }
+
+    // Called at end of round to clear out players' hands
+    const resetHand = () => {
+      this.hand = []
+    }
 
 }
