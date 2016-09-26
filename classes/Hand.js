31dae7524c7a2d1a5303aa2872bfb6035cad2167
fryymann715 <ideans715@gmail.com>
2016-09-26 15:56:33 -0700

    Tweaked Hand class and Player class

--- classes/Hand.js
+++ classes/Hand.js
@@ -2,10 +2,17 @@ class Hand {
   constructor () {
     this.cards = []
     this.currentBet = 0
+    this.player = // Variable mapped to Player class?
   }
 
   const addCard = ( card ) => {
     this.cards.push( card )
   }
+
+  const increaseBet = ( amount ) => {
+    this.currentBet += amount
+  }
+
+
 
 }
--- classes/Player.js
+++ classes/Player.js
@@ -1,6 +1,6 @@
 class Player {
-    constructor(options) {
-    this.hand = // Instance of Hand class
+    constructor( options ) {
+    this.hands =  [] // Instance of Hand classes stored in array
     this.bank = 100
     this.seat = options.seat
     }
@@ -8,37 +8,30 @@ class Player {
     // Player decides to add a new card to their hand.
     const hit = () => {
       // invokes dealCard() on the dealer which invokes addCard() on the hand.
-    },
+    }
 
     // Takes funds from player's bank and adds it the the Hand's bet variable.
-    const bet = ( option ) => {
-      if ( option = 'all-in' ) {
-        // Add this.bank to Hand's bet.
-      } else {
-        this.currentBet = option
-        this.bank -= option
+    const bet = option => {
+      for ( let hand in this.hands ) {
+        hand.increaseBet( option )
       }
     }
 
-    // If player wishes to sit out a round.
+    // Player passes on taking another card.
     const stay = () => {
 
     }
 
-    // Doubles the player's current bet
+    // Takes the current bet from a Players Hand and doubles it.
     const doubleDown = () => {
-      let doubledBet = this.currentBet * 2
+      let doubledBet = this.hand.currentBet * 2
       this.bank -= doubledBet
-    },
+    }
 
-    // If player is deal two of the same cards, splits hand into two hands.
+    // If player is dealt two of the same cards, splits hand into two hands.
     const split = () => {
-      if ( this.hand[0].rank === this.hand[1].rank ) {
-        let hand1 = [this.hand[0]]
-        let hand2 = [this.hand[1]]
-        this.hand = [hand1, hand2]
-      } else {
-        return "Can only split when deal two of the same cards."
+      if ( this.hand.cards[0].rank === this.hand.cards[1].rank ) {
+
       }
     }
 
@@ -47,15 +40,15 @@ class Player {
 
     }
 
-    //
+    // If Player wishes to sit out a round.
     const surrender = () => {
 
     }
 
     // Calls the Card.value() function to total the value of Player's hand.
-    const handValue = (this.hand) => {
+    const handValue = ( handObject ) => {
       let total = 0
-      for ( let card in hand ) {
+      for ( let card in handObject ) {
         total += card.value()
       }
       return total
@@ -63,7 +56,11 @@ class Player {
 
     // Called at end of round to clear out players' hands
     const resetHand = () => {
-      this.hand = []
+      while( this.hands.length > 0 ){
+          this.hands.pop()
+      }
     }
 
 }
+
+module.exports = Player
