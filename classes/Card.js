aec4bf54d39724c2e5ece64254730f71259a6626
Abraham Ferguson <abrahamferguson@Abrahams-MacBook-Pro.local>
2016-09-26 17:48:10 -0700

    Card class is complete, Deck is work in progress

--- classes/Card.js
+++ classes/Card.js
@@ -1,16 +1,67 @@
-class Card {
+  const SPADES = '♠️'
+  const CLUBS = '♣️'
+  const HEARTS = '♥️'
+  const DIAMONDS = '♦️'
+  const JACK = 'J'
+  const QUEEN = 'Q'
+  const KING = 'K'
+  const ACE = 'A'
 
-    constructor() {
-        this.suit 
-        this.rank 
-                
+  const SUITS = [SPADES, CLUBS, HEARTS, DIAMONDS]
+  const RANKS = [2, 3, 4, 5, 6, 7, 8, 9, JACK, QUEEN, KING, ACE]
+  const VALUES = {
+    [JACK]:   10,
+    [QUEEN]:  10,
+    [KING]:   10,
+    [ACE]:  1 || 11
+  }
+
+class Card{
+
+    constructor(rank, suit) {
+
+        this.suit = suit
+        this.rank = rank
+
     }
-    
-    isAce(),
-    
-    value()
-    
+
+     isAce(){
+      return this.rank === ACE
+    }
+
+    value(){
+      return VALUES[ this.rank ] || [ this.suit ]
+    }
+
+    toString(){
+      return `${this.rank}${this.suit}`
+    }
 }
 
+    Card.all = function(){
+      const cards = []
+      SUITS.forEach(suit =>{
+        RANKS.forEach(rank =>{
+          cards.push(new Card( rank, suit ))
+        })
+      })
+      return cards
+
+    }
 
 
+
+
+ Card.SUITS     = SUITS
+ Card.RANKS     = RANKS
+ Card.HEARTS    = HEARTS
+ Card.DIAMONDS  = DIAMONDS
+ Card.CLUBS     = CLUBS
+ Card.SPADES    = SPADES
+ Card.ACE       = ACE
+ Card.JACK      = JACK
+ Card.QUEEN     = QUEEN
+ Card.KING      = KING
+
+
+module.exports = Card
