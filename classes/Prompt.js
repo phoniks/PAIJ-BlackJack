26bd61f852ec8b721b441d7271fdc2b5c8cae034
Abraham Ferguson <abrahamferguson@Abrahams-MacBook-Pro.local>
2016-09-28 14:24:44 -0700

    Changes to AIPlayer, Dealer, Prompt, testPlayer

--- classes/Prompt.js
+++ classes/Prompt.js
@@ -1,9 +1,11 @@
-
+const rl = require('readline-sync')
+const chalk = require('chalk')
+const color = require('color')
 
 module.exports = {
   ask: (question) =>{
     return rl.question(chalk.underline.bgBlue('>>' + question) + '')
-  }
+  },
 
   askForNumber: (question) =>{
     return parseInt(this.ask(question), 10)
